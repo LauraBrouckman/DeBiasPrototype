c37f093d7cb6510fc37097382d1e9a357f1f424d
Elizabeth Brouckman <elizabethbrouckman@DN0a2461d3.SUNet>
2016-11-18 10:05:55 -0800

    THaehg

--- DeBias/Colors.swift
+++ DeBias/Colors.swift
@@ -11,7 +11,7 @@
 import UIKit
 
 class Colors {
-    static let darkRed = UIColor(red: 245.0/255, green: 37.0/255, blue: 37.0/255, alpha: 1.0)
+    static let darkRed = UIColor(red: 245.0/255, green: 36.0/255, blue: 37.0/255, alpha: 1.0)
     static let lightRed = UIColor(red: 250.0/255, green: 92.0/255, blue: 92.0/255, alpha: 1.0)
     static let purple = UIColor(red: 178.0/255, green: 61.0/255, blue: 224.0/255, alpha: 1.0)
     static let lightBlue = UIColor(red: 88.0/255, green: 88.0/255, blue: 219.0/255, alpha: 1.0)
