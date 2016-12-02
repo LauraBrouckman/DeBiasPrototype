9bbfcafa6fc7cbe4dbaa00fbf179d3c20e6bf2f0
Leslie Kurt <lesliekurt@DN51ut2i.SUNet>
2016-12-01 17:31:14 -0800

    Updated toolbar and topbar of friends screen

---
+++ DeBias/FriendToolbarViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  FriendToolbarViewCell.swift
+//  DeBias
+//
+//  Created by Leslie Kurt on 11/29/16.
+//  Copyright © 2016 debias. All rights reserved.
+//
+
+import UIKit
+
+class FriendToolbarViewCell: UITableViewCell {
+    
+
+    @IBOutlet weak var addFriendButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    
+}
+
